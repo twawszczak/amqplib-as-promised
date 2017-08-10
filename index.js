00b4e69dd06c348d35e0d8ee62f42894b6fd63e2
Tadeusz Wawszczak <tadeusz.wawszczak@onet.eu>
2017-08-10 04:27:20 -0400

    connection and channel wrapper

---
+++ index.js
@@ -0,0 +1,55 @@
+let amqplib = require('amqplib')
+
+class AmqplibAsPromised {
+  static wrap (object) {
+    amqplib = object
+    return this
+  }
+
+  static connect (url, socketOptions) {
+    return amqplib.connect(url, socketOptions)
+      .then(connection => AmqplibConnectionWrapper.wrap(connection)
+      )
+  }
+}
+
+AmqplibAsPromised.credentials = amqplib.credentials
+
+class AmqplibConnectionWrapper {
+  static wrap (connection) {
+    const createChannel = connection.createChannel.bind(connection)
+    connection.createChannel = () => {
+      return createChannel()
+        .then(channel => AmqplibChannelWrapper.wrap(channel))
+    }
+    return connection
+  }
+}
+
+class AmqplibChannelWrapper {
+  static wrap (channel) {
+    const sendToQueue = channel.sendToQueue.bind(channel)
+    channel.sendToQueue = (queue, content, options) => {
+      return new Promise((resolve, reject) => {
+        const canSend = sendToQueue(queue, content, options)
+        if (canSend) {
+          resolve()
+        } else {
+          try {
+            channel.once('drain', () => {
+              resolve()
+            })
+            channel.once('error', (error) => {
+              reject(error)
+            })
+          } catch (error) {
+            reject(error)
+          }
+        }
+      })
+    }
+    return channel
+  }
+}
+
+module.exports = AmqplibAsPromised
