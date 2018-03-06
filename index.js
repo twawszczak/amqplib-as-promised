4e2e1ac4c76e4ea9804129cf7aa3ae40ef4c4857
Tadeusz Wawszczak <Tadeusz.Wawszczak@mapp.com>
2018-03-06 11:56:08 +0100

    build

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var connection_1 = require("./lib/connection");
 exports.Connection = connection_1.Connection;
+var channel_1 = require("./lib/channel");
+exports.Channel = channel_1.Channel;
