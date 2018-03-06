a2a0640e277418c5afea482b33e96e8c64e7bace
Tadeusz Wawszczak <Tadeusz.Wawszczak@mapp.com>
2018-03-06 14:26:51 +0100

    build

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-var connection_1 = require("./lib/connection");
+const connection_1 = require("./lib/connection");
 exports.Connection = connection_1.Connection;
-var channel_1 = require("./lib/channel");
+const channel_1 = require("./lib/channel");
 exports.Channel = channel_1.Channel;
+__export(require("amqplib"));
