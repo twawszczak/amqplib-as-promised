641d358f1b8926a2ce7166dbd3456bed70e36597
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-03-12 19:16:32 +0100

    tslib is not required when we do not use export *

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
 const channel_1 = require("./lib/channel");
 exports.Channel = channel_1.Channel;
 const connection_1 = require("./lib/connection");
 exports.Connection = connection_1.Connection;
-tslib_1.__exportStar(require("amqplib"), exports);
+var amqplib_1 = require("amqplib");
+exports.credentials = amqplib_1.credentials;
+exports.connect = amqplib_1.connect;
